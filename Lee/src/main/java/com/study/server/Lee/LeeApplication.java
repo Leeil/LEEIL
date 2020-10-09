ddab34ac5bc0ef032cbe9e68e2e871952980774c
Leeli <likyeong961@naver.com>
2020-10-10 02:58:52 +0900

    Ex1-2

--- Lee/src/main/java/com/study/server/Lee/LeeApplication.java
+++ Lee/src/main/java/com/study/server/Lee/LeeApplication.java
@@ -1,18 +1,13 @@
 package com.study.server.Lee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
- 
-@RestController
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LeeApplication {
-    @RequestMapping("/")
-    public @ResponseBody String root_test()throws Exception{
-        return "study server Lee Page";
-    }
- 
-    @RequestMapping("/demo")
-    public @ResponseBody String demo_test()throws Exception{
-        return "이일경 페이지에 접속했습니다.";
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeApplication.class, args);
+	}
+
 }
