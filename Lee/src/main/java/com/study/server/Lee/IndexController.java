b79160a7f1745e960c15708d7efaf770d89a971a
Leeli <likyeong961@naver.com>
2020-10-10 14:00:48 +0900

    Ex1-4

--- Lee/src/main/java/com/study/server/Lee/IndexController.java
+++ Lee/src/main/java/com/study/server/Lee/IndexController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+    @GetMapping({"",  "/"})
     public String index(){
+    	System.out.println("test");
         return "index";
     }
 }
